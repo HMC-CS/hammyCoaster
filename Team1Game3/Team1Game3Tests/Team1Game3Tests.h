d9210dab2884f98bc1d5e9259ad1742d4648a4df
Team 1 <claire.ma.murphy@gmail.com>
2013-03-24 16:25:47 -0700

    figuring out test import problems
    When I try to run the tests, XCode cannot seem to find some built-in
    files (like kazmath and Box2D).  I tried fiddling with the paths to fix
    the kazmath problem, but the same didn't work for Box2D, and I'm a
    little lost.

---
+++ Team1Game3/Team1Game3Tests/Team1Game3Tests.h
@@ -0,0 +1,45 @@
+//
+//  Team1Game3Tests.h
+//  Team1Game3Tests
+//
+//  Created by jarthur on 3/24/13.
+//
+//
+
+// TODO - organize files into folders so imports become easier!
+
+#import <SenTestingKit/SenTestingKit.h>
+
+#import "AbstractGameObject.h"
+#import "BallObject.h"
+#import "RampObject.h"
+#import "BluePortalObject.h"
+
+#import "LevelLayer.h"
+#import "InventoryLayer.h"
+#import "PhysicsLayer.h"
+#import "MainMenuLayer.h"
+
+//#import "ContactListener.h"
+//#import "ObjectFactory.h"
+
+#import "cocos2d.h"
+#import "Box2D.h"
+
+@interface Team1Game3UnitTests : SenTestCase {
+    
+    BallObject* ball;
+    RampObject* ramp;
+    BluePortalObject* bluePortal;
+    AbstractGameObject* abstractBall;
+    
+    LevelLayer* levelLayer;
+    InventoryLayer* inventoryLayer;
+    PhysicsLayer* physicsLayer;
+    MainMenuLayer* mainMenuLayer;
+    
+}
+
+- (void) checkNotNil;
+
+@end
