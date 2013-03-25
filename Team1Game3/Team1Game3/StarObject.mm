1bdcdb1abd781828bb7ff5fa6c68f07a2e95967b
Team 1 <claire.ma.murphy@gmail.com>
2013-03-24 21:22:56 -0700

    made stars a sensor
    also learned that you can't make concave polygons in box2d

--- Team1Game3/Team1Game3/StarObject.mm
+++ Team1Game3/Team1Game3/StarObject.mm
@@ -18,37 +18,28 @@
     _bodyDef.position.Set(location.x/PTM_RATIO, location.y/PTM_RATIO);
     b2Body *star_Body = _world->CreateBody(&_bodyDef);
     
-    b2Vec2 vertices[4];
-    //row 1, col 1
-    int num = 4;
-    vertices[0].Set(-26.1f / PTM_RATIO, 27.0f / PTM_RATIO);
-    vertices[1].Set(-25.9f / PTM_RATIO, -24.9f / PTM_RATIO);
-    vertices[2].Set(26.2f / PTM_RATIO, -24.9f / PTM_RATIO);
-    vertices[3].Set(26.2f / PTM_RATIO, 27.1f / PTM_RATIO);
+    b2Vec2 vertices[5];
+    int num = 5;
+    vertices[0].Set(-0.1f / PTM_RATIO, 16.0f / PTM_RATIO);
+    vertices[1].Set(-14.8f / PTM_RATIO, 2.4f / PTM_RATIO);
+    vertices[2].Set(-10.0f / PTM_RATIO, -16.4f / PTM_RATIO);
+    vertices[3].Set(11.2f / PTM_RATIO, -16.8f / PTM_RATIO);
+    vertices[4].Set(18.8f / PTM_RATIO, 1.6f / PTM_RATIO);
     
+
+
     
     b2PolygonShape starShape;
     starShape.Set(vertices, num);
     
     //b2FixtureDef _fixtureDef;
     _fixtureDef.shape = &starShape; // Set the line shape
-    _fixtureDef.density = 100.0f; // Set the density
-    _fixtureDef.friction = 0.5f; // Set the friction
-    _fixtureDef.restitution = 0.1f; // Set the restitution
+    _fixtureDef.isSensor = TRUE; // make the star a sensor (no interaction with other bodies)
     
     // Add the shape to the body
     star_Body->CreateFixture(&_fixtureDef);
     star_Body->SetUserData(self);
-    //b2Fixture->SetUserData("bluePortal");[/code]
-    //b2CircleShape circle;
-    //circle.m_radius = 26.0/PTM_RATIO;
-    
-    //_fixtureDef.shape = &circle;
-    //_fixtureDef.density = 1.0f;
-    //_fixtureDef.friction = 0.4f;
-    //_fixtureDef.restitution = 0.3f;
-    
-    //body->CreateFixture(&_fixtureDef);
+ 
     return star_Body;
 }
 
