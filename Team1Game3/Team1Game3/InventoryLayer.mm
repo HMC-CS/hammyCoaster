785a706feacfee9e649979c19385b1c8fab13720
cramsden <cramsden@hmc.edu>
2013-04-03 23:51:50 -0700

    got it to highlight for a second awks

--- Team1Game3/Team1Game3/InventoryLayer.mm
+++ Team1Game3/Team1Game3/InventoryLayer.mm
@@ -52,8 +52,9 @@
         // int numItems = [[item objectAtIndex:2] intValue];   // Number of inventory items, when needed
         
         NSString* buttonLabel = [[NSString alloc] initWithFormat:@"%@Icon.png", label];
+        NSString* selectedButtonLabel = [[NSString alloc] initWithFormat:@"%@IconSelected.png", label];
 
-        CCMenuItemImage *inventoryButton = [CCMenuItemImage itemFromNormalImage:buttonLabel selectedImage:buttonLabel disabledImage:buttonLabel block:^(id sender)
+        CCMenuItemImage *inventoryButton = [CCMenuItemImage itemFromNormalImage:buttonLabel selectedImage:selectedButtonLabel disabledImage:buttonLabel block:^(id sender)
         {
             //CCMenuItemImage *button = (CCMenuItemImage *)sender;
         
