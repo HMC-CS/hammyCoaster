9849fe511b27b9b4ea10f02f1184f36e7e441150
Team 1 <claire.ma.murphy@gmail.com>
2013-04-05 00:56:14 -0700

    Revert "tried to do ramps, committing so it'll let me revert"
    This reverts commit 965c531ecd433e05d3edb9904eabfa09b036e60f.

--- Team1Game3/Team1Game3/PhysicsLayer.mm
+++ Team1Game3/Team1Game3/PhysicsLayer.mm
@@ -134,9 +134,6 @@
     /* hacked ball starting position
      * ---------------------------------------------------------------------- */
     ballStartingPoint = CGPointMake(15.0, 600.0);
-    
-    //-- curved ramp test code --//
-    //addNewSpriteOfType:CurvedRampObject AtPosition:CGPointMake(150.0, 600.0) WithRotation:2 AsDefault:YES;
 }
 
 - (void) addInitialObjects
