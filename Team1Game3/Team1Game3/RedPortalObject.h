a3c2f4a30d98dbef93640a00d7e4b08381ef7f8c
Michelle Chesley <mmchesley@gmail.com>
2013-04-08 16:18:52 -0700

    Start portal!

---
+++ Team1Game3/Team1Game3/RedPortalObject.h
@@ -0,0 +1,14 @@
+//
+//  RedPortalObject.h
+//  Team1Game3
+//
+//  Created by jarthur on 3/14/13.
+//
+//
+
+#import "AbstractGameObject.h"
+
+@interface RedPortalObject : AbstractGameObject
+
+
+@end
---
+++ Team1Game3/Team1Game3/RedPortalObject.mm
@@ -0,0 +1,55 @@
+//
+//  RedPortalObject.m
+//  Team1Game3
+//
+//  Created by jarthur on 3/14/13.
+//
+//
+
+#import "RedPortalObject.h"
+
+@implementation RedPortalObject
+
+- (b2Body *)createBody:(CGPoint)location {
+    
+    //b2BodyDef bluePortalBodyDef;
+    _bodyDef.type = b2_staticBody;
+    _bodyDef.position.Set(location.x/PTM_RATIO, location.y/PTM_RATIO);
+    b2Body *redPortal_Body = _world->CreateBody(&_bodyDef);
+    
+    b2Vec2 vertices[4];
+    //row 1, col 1
+    int num = 4;
+    vertices[0].Set(-26.1f / PTM_RATIO, 27.0f / PTM_RATIO);
+    vertices[1].Set(-25.9f / PTM_RATIO, -24.9f / PTM_RATIO);
+    vertices[2].Set(26.2f / PTM_RATIO, -24.9f / PTM_RATIO);
+    vertices[3].Set(26.2f / PTM_RATIO, 27.1f / PTM_RATIO);
+    
+    
+    b2PolygonShape redPortalShape;
+    redPortalShape.Set(vertices, num);
+    
+    //b2FixtureDef _fixtureDef;
+    _fixtureDef.shape = &redPortalShape; // Set the line shape
+    _fixtureDef.density = 100.0f; // Set the density
+    _fixtureDef.friction = 0.5f; // Set the friction
+    _fixtureDef.restitution = 0.1f; // Set the restitution
+    _fixtureDef.isSensor = TRUE; // make the star a sensor (no interaction with other bodies)
+    
+    // Add the shape to the body
+    redPortal_Body->CreateFixture(&_fixtureDef);
+    redPortal_Body->SetUserData(self);
+    //b2Fixture->SetUserData("bluePortal");[/code]
+    //b2CircleShape circle;
+    //circle.m_radius = 26.0/PTM_RATIO;
+    
+    //_fixtureDef.shape = &circle;
+    //_fixtureDef.density = 1.0f;
+    //_fixtureDef.friction = 0.4f;
+    //_fixtureDef.restitution = 0.3f;
+    
+    //body->CreateFixture(&_fixtureDef);
+    return redPortal_Body;
+}
+
+@end
