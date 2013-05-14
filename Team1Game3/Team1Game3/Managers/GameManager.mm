f54ec5ab96b2079f492cf2f13adf8afa059511b7
Team 1 <claire.ma.murphy@gmail.com>
2013-05-14 03:12:49 -0700

    syncing

--- Team1Game3/Team1Game3/Managers/GameManager.mm
+++ Team1Game3/Team1Game3/Managers/GameManager.mm
@@ -19,7 +19,7 @@
         [self loadGame];
         
         // Control the number of levels with these variables.
-        _numLevelSets = 2;
+        _numLevelSets = 1;
         _numLevelIndices = 12;
         
         
