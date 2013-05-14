670a4ba009bded8c8c68203367575c552e29d3f0
cramsden <cramsden@hmc.edu>
2013-05-13 21:13:23 -0700

    commenting

--- Team1Game3/SpriteResizingFunctions.mm
+++ Team1Game3/SpriteResizingFunctions.mm
@@ -15,11 +15,16 @@
 
 @implementation SpriteResizingFunctions
 
+/* setSpritSize inLayer: withSize:
+ * scales the sprite based on the layer and the fraction of the layer you want the size of
+ * the object to be, then sets the contentSize to the size of the new bounding box
+ */
+
 + (void) setSpriteSize: (CCSprite*) sprite inLayer: (CCLayer*) layer withSize: (CGFloat) size;
 {
     //[sprite setTextureRect:CGRectMake(x,y,width,height)]
     CGFloat newWidth = size * layer.boundingBox.size.width;
-    CGFloat newHeight = size * (layer.boundingBox.size.height/4);
+    CGFloat newHeight = size * (layer.boundingBox.size.height/3);
     [sprite setScaleX: (newWidth/sprite.contentSize.width)];
     [sprite setScaleY: (newHeight/sprite.contentSize.height)];
     sprite.contentSize = sprite.boundingBox.size;
