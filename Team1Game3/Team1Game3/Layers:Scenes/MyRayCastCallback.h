4a11fbc716a83bdfc271b370f53320a6c3663385
jeansung <jsung@d41-60.cs.hmc.edu>
2013-06-25 16:27:56 -0700

    object overlap works!!! need to add exceptions for stars, blue portal etc need to color grey/recolor to original need to break finishedMovingObject into methods (see 7 closing parens) :)

---
+++ Team1Game3/Team1Game3/Layers:Scenes/MyRayCastCallback.h
@@ -0,0 +1,15 @@
+//
+//  MyRayCastCallback.h
+//  Team1Game3
+//
+//  Created by Jean Sung on 6/25/13.
+//
+//
+
+#ifndef __Team1Game3__MyRayCastCallback__
+#define __Team1Game3__MyRayCastCallback__
+
+#include <iostream>
+#include "PhysicsLayer.h"
+
+#endif /* defined(__Team1Game3__MyRayCastCallback__) */
